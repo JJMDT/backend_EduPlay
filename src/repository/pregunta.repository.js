6fae33fe9ad8d3a9add20bba6bf362514b475fac
LisettPruebas <lisett.pruebas@gmail.com>
2025-06-10 00:37:30 -0300

    Agregué método para preguntas aleatorias en el get

--- src/repository/pregunta.repository.js
+++ src/repository/pregunta.repository.js
@@ -2,11 +2,10 @@ const preguntas = require('../DB/db.json')
 
 exports.getPreguntaRepository = async (req, res) => {
   try {
-    let pregunta = preguntas.find(p => p.id == 1);
-    console.log(pregunta)
+    const randomNumber = Math.floor(Math.random() * preguntas.length) + 1;
+    let pregunta = preguntas.find(p => p.id == randomNumber);
     return  pregunta
   } catch (err) {
-    console.log("asdasd")
     res.status(500).json({ error: err.message });
   }
 };
