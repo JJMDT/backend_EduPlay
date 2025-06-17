b8fdebf016f606dfc49d57724205ed71f7c2c2fd
JJMDT <127325507+JJMDT@users.noreply.github.com>
2025-06-17 20:45:26 -0300

    Update pregunta.Controller.js
    arreglo de merge

--- src/controllers/pregunta.Controller.js
+++ src/controllers/pregunta.Controller.js
@@ -1,13 +1,14 @@
 const services = require('../services/preguntas.services.js')
-//clau//
 
-exports.eliminarPregunta = async (req,res) => {
-    try{
+exports.getPreguntaController = async (req, res) => {
+  try {
+    const preguntas = await services.getPreguntaServices();
+    res.json(preguntas);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-
-        const id = req.params.id;
-        const pregunta = await services.EliminarPreguntasServices(id)
-        console.log("exitoso desde controler")
 exports.getPreguntaByIdController = async (req, res) => {
   try {
     const id = parseInt(req.params.id);
@@ -21,14 +22,14 @@ exports.getPreguntaByIdController = async (req, res) => {
   }
 };
 
-
-    } catch (err) {
-
-        console.log("asdasd")
-        res.status(500).json({ error: err.message });
-
-    }
-}
+exports.getRespuestaByIdController = async (req, res) => {
+  try {
+    const respuesta = await getRespuestaByIdService(req.params.id);
+    res.json(respuesta);
+  } catch (err) {
+    res.status(400).json({ error: 'No encontrada' });
+  }
+};
 
 
 exports.putPreguntaController = async (req, res) => {
