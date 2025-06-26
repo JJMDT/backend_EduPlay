6ebe0e2f831fa3461b0300bfa2a620845caed4f9
CLauchanchisan <claudia_casalone@hotmail.com>
2025-06-25 22:12:17 -0300

    Update auth.service.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -7,3 +7,4 @@ exports.loginService = async ({username,password}) => {
     }
     return { message: 'Login exitoso', user };
 };
+//En este servicio de autenticación, recibimos las credenciales y consultamos en la base de datos si el usuario existe. Si no existe, lanzamos un error que luego podemos manejar para informar al usuario. Si el usuario es válido, devolvemos un mensaje de éxito junto con su información para continuar con la sesión.
