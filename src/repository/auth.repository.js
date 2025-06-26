6d2cd472dcdb2b4bb243ba21f088c210ac7e5664
CLauchanchisan <claudia_casalone@hotmail.com>
2025-06-25 22:43:05 -0300

    Update auth.repository.js

--- src/repository/auth.repository.js
+++ src/repository/auth.repository.js
@@ -21,3 +21,4 @@ exports.findUserByUsername = async (username, password) => {
 
 //La contraseña no se compara directamente, sino con una función segura (hash).
 //Esta función busca un usuario por su nombre y luego verifica que la contraseña sea correcta usando un método seguro de comparación. Si el usuario no existe o la contraseña es incorrecta, devuelve null. De lo contrario, devuelve el usuario para continuar con el proceso de autenticación.
+//La capa repositorio es el puente entre nuestra aplicación y la base de datos. Aquí están las funciones que saben cómo guardar, buscar, actualizar o borrar datos usando los modelos. Mantener esta capa separada ayuda a que nuestra aplicación sea más limpia y fácil de modificar.
