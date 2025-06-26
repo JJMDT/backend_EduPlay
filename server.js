d133585c9bc780fe7aa4c460fe5e739c3cecfb95
CLauchanchisan <claudia_casalone@hotmail.com>
2025-06-25 21:57:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,13 +15,16 @@ const { getConnection } = require('./src/DB/conexion');
 //PORT y HOST: Definen la dirección y el puerto donde corre tu servidor.
 
 getConnection();
+//Importás una función para conectarte a la base de datos y la ejecutás apenas inicia el servidor.
+//esto asegura que el servidor no arranca si la base de datos no responde.
 app.use(cors());
-
+//cors(): Habilita las solicitudes desde otros dominios (como tu frontend).
 const routerAuth = require('./src/routes/auth.routes');
 const routerPreguntas = require('./src/routes/preguntas.routes');
 const routerScore = require('./src/routes/score.routes');
 
 app.use(express.json());   
+//express.json(): Permite que Express entienda los cuerpos JSON en las peticiones (por ejemplo, cuando hacés un POST desde el frontend
 
 app.use('/auth', routerAuth);
 app.use('/pregunta', routerPreguntas);
@@ -38,3 +41,4 @@ app.use((req, res) => {
 app.listen(PORT, HOST, () => {
   console.log(`Servidor corriendo http://${HOST}:${PORT}`);
 });
+//Este archivo es el corazón del backend de EduPlay. Utiliza Express para levantar un servidor en el puerto 3000 y conectar con la base de datos. Está dividido en tres rutas principales: autenticación, preguntas y puntajes. Cada una de ellas está organizada en archivos separados para mantener el código limpio y modular. También incluye seguridad básica con CORS y una ruta para manejar errores 404. Finalmente, el servidor queda escuchando en localhost para recibir peticiones del frontend.
