bf9a576496d41085868ae6c212fcc181de2033ef
CLauchanchisan <claudia_casalone@hotmail.com>
2025-06-25 22:41:35 -0300

    Update auth.repository.js

--- src/repository/auth.repository.js
+++ src/repository/auth.repository.js
@@ -15,3 +15,9 @@ exports.findUserByUsername = async (username, password) => {
         throw error;
     }
 };
+//Aquí está la validación real del usuario y contraseña.
+
+//Permite que el servicio de login sepa si el usuario es válido o no.
+
+//La contraseña no se compara directamente, sino con una función segura (hash).
+//Esta función busca un usuario por su nombre y luego verifica que la contraseña sea correcta usando un método seguro de comparación. Si el usuario no existe o la contraseña es incorrecta, devuelve null. De lo contrario, devuelve el usuario para continuar con el proceso de autenticación.
