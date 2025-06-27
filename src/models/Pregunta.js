3f77a502ad5106a26e13c8988858052f928e24d2
CLauchanchisan <claudia_casalone@hotmail.com>
2025-06-26 22:36:25 -0300

    Update Pregunta.js

--- src/models/Pregunta.js
+++ src/models/Pregunta.js
@@ -18,6 +18,6 @@ const preguntaSchema = new Schema({
   timestamps: true,
   versionKey: false
 });
-
+//Este modelo define cómo se guarda una pregunta en la base de datos. Cada pregunta tiene un enunciado, una lista de opciones y la respuesta correcta. Todo eso se guarda con Mongoose en MongoDB. Además, se registran automáticamente las fechas de creación y modificación. Este modelo se usa para crear, mostrar, editar o eliminar preguntas en la aplicación.
 const Pregunta = mongoose.model('Pregunta', preguntaSchema);
 module.exports = Pregunta;
