5dd4206e6dc0dce3df2647c2f7a30b7c6940b388
CLauchanchisan <claudia_casalone@hotmail.com>
2025-07-01 18:41:19 -0300

    a

--- src/models/user.js
+++ src/models/user.js
@@ -18,8 +18,8 @@ const userSchema = new Schema({
 });
 
 //userSchema.methods.comparePassword = async function(candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
-};
+//     return await bcrypt.compare(candidatePassword, this.password);
+// };
 //Define un método que se puede usar en cualquier instancia de usuario para comparar una contraseña ingresada (candidatePassword) con la contraseña almacenada (this.password).
 
 //Usa bcrypt.compare para hacer una comparación segura (hashing).
